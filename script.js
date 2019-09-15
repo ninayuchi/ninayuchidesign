1c53ca44be0d9d7af21f2ec6a99bfd915e6c284e
Nina Y <nyuchi@risd.edu>
2019-09-15 16:52:58 -0400

    link fixed

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 var myArray = [
-  'a DMV-based graphic designer.',
+  'a DMV-based designer.',
   'an amateur tofu chef.',
   'a tiger in the Chinese zodiac.',
   'an Aquarius in the Western zodiac.',
   'a dog person (and allergic to cats).',
   'currently listening to <a href="https://open.spotify.com/album/6K7SjlIebufubxXGf4uA7a?si=_WQqGTq6QrKPanIHrADRcw" target="_blank">Live on Ice.</a>',
-  'a graphic design student at RISD.'
+  'a graphic design student at RISD.',
+  'an annual viewer of the Perseid meteor showers (since 2015!).'
 ];
 
 var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
