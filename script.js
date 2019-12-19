3787110850e27a3337db5cf5e1980691bdac0e1c
Nina Y <nyuchi@risd.edu>
2019-12-19 16:02:22 -0500

    unesco

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var myArray = [
-  'a DMV-based designer.',
   'an amateur tofu chef.',
   'a tiger in the Chinese zodiac.',
   'an Aquarius in the Western zodiac.',
   'a dog person (and allergic to cats).',
   'currently listening to <a href="https://open.spotify.com/track/5Kz1ZTkMQjhx0Chvmaoxzv?si=1YWNH5kBQwWnWfRPy_1E9Q" target="_blank">RFYL.</a>',
   'a graphic design student at RISD.',
-  'an annual viewer of the Perseid meteor showers.',
-  'currently a redhead (soon to change).',
+  'thinking about her next hair color.',
+  'currently blonde (soon to change).',
   'hydrated.',
-  'thinking about her several different Halloween costumes.'
+  'thinking about Christmas songs.',
+  'currently playing Fire Emblem.'
 ];
 
 var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
