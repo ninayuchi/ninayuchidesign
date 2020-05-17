032ab5cb07030e7cafae0fa60c44ba93465bbc96
Nina Y <nyuchi@risd.edu>
2020-05-17 16:31:21 -0400

    cleanup

--- deconstructing-discipline/script.js
+++ deconstructing-discipline/script.js
@@ -92,23 +92,23 @@ $(function(){
 
 });
 
-function createPDF() {
-	var doc = new jsPDF({ orientation: 'landscape'});
-	var elementHTML = $('canvas').html();
-	var specialElementHandlers = {
-		'elementH': function(element, renderer){
-			return true;
-		}
-	};
+// function createPDF() {
+// 	var doc = new jsPDF({ orientation: 'landscape'});
+// 	var elementHTML = $('canvas').html();
+// 	var specialElementHandlers = {
+// 		'elementH': function(element, renderer){
+// 			return true;
+// 		}
+// 	};
 
-	doc.fromHTML(elementHTML, 0,0, {
-		'width': 170,
-		'elementHandlers': specialElementHandlers
-	});
+// 	doc.fromHTML(elementHTML, 0,0, {
+// 		'width': 170,
+// 		'elementHandlers': specialElementHandlers
+// 	});
 
-	doc.save('myMap.pdf');
+// 	doc.save('myMap.pdf');
 
-}
+// }
 
 function generatePDF(){
 	html2canvas(document.querySelector("#body"), { 
@@ -120,36 +120,10 @@ function generatePDF(){
 
     var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
     image.crossOrigin = "Anonymous";
-    console.log(image);
     window.location.href=image;
 
 	});
 }
-
-// function saveAs(uri, filename) {
-
-//     var link = document.createElement('a');
-
-//     if (typeof link.download === 'string') {
-
-//         link.href = uri;
-//         link.download = filename;
-
-//         //Firefox requires the link to be in the body
-//         document.body.appendChild(link);
-
-//         //simulate click
-//         link.click();
-
-//         //remove the link when done
-//         document.body.removeChild(link);
-
-//     } else {
-
-//         window.open(uri);
-
-//     }
-// }
 
 
 
